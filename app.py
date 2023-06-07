d0d05386c37ea3e3a0a6aac897d8ae12b905c412
Maaz-LFD <maaz.rafiq@lovefordata.com>
2023-06-07 16:11:17 +0500

    adding spell_grammar_func, requirements.txt, essay.tsv

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ from flask import Flask, request
 from sentence_transformers import SentenceTransformer
 import spacy
 import pymongo
-from utils import Preprocess, Title_Essay_Relevancy,plag_calculate
+from utils import Preprocess, Title_Essay_Relevancy,plag_calculate,Grammar_Spell_Check
 from strsimpy import Cosine
+from happytransformer import HappyTextToText, TTSettings
 
 # Intialization
 cosine_1g, cosine_2g, cosine_3g = Cosine(1), Cosine(2), Cosine(3)
@@ -21,13 +22,16 @@ model_title_essay = SentenceTransformer('multi-qa-MiniLM-L6-cos-v1')
 # Data Processing English language model in spaCy
 nlp = spacy.load("en_core_web_sm") #exclude=['tagger','parser','ner','entity_linker']
 
+# Grammar and spelling correction model
+happy_tt = HappyTextToText("T5", "vennify/t5-base-grammar-correction")
+happy_tt_args = TTSettings(num_beams=3, min_length=1)
+
 
 app = Flask(__name__)
 
 @app.route('/process', methods=['POST'])
 def process_file():
-    print("request.files ",request.files)
-    # Check if the request contains a file
+
     if 'file' not in request.files:
         return 'Error: No file uploaded.'
 
@@ -36,29 +40,48 @@ def process_file():
     # Check if the file is empty
     if file.filename == '':
         return 'Error: Empty file uploaded.'
+    
+    # Get the optional parameters
+    plag_thresh = float(request.form.get('plag_thresh', default=0.5))
+    grammar_thresh = float(request.form.get('grammar_thresh', default=0.2))
+    title = request.form.get('title', default=None)
+    # print("parameters \n",plag_thresh,grammar_thresh,title)
 
     try:
         # Read the contents of the file
-        file_contents = file.read().decode('utf-8')
-        print("type(file_contents) ",type(file_contents))
-        print(file_contents)
+        file_content = file.read().decode('utf-8')
+        # print("type(file_content) ",type(file_content))
+        # print(file_content)
 
-        # Spliting Title and essay
-        split_content = file_contents.split('\r\n\r\n\r\n')
-        title = split_content[0]; essay = split_content[1]
-        print('title ',title)
-        print('essay ',essay)
+        # Extracting title from text file 
+        if title is None:
+            # Spliting Title and essay
+            split_content = file_content.split('\n\n\n')
+            title = split_content[0]; essay = split_content[1]
+            # print('title ',title)
+            # print('essay ',essay)
+        
+        else:
+            essay = file_content
 
-        # Applying Preprocessing for plagiarism calaculcation
+        #Grammar and Spelling Check
+        grammar_result = Grammar_Spell_Check(text_process_pipeline=nlp, gs_model=happy_tt, gs_model_args=happy_tt_args, cosine_obj=cosine_1g, essay=essay)
+        print("grammar_result ",grammar_result)
+
+        # Preprocessing for plagiarism calaculcation
         preprocessed_essay = Preprocess(nlp,essay)
         
-        if len(essays_list) == 0:
-            print('No Plag Found')
-        else:
+        # Plag result high result means low plag
+        if len(essays_list) != 0:
             plag_result = plag_calculate(cosine_1g, cosine_2g, cosine_3g, essay, db_essay_list=essays_list)
+        else:
+            plag_result = 1
+        
+        print("plag result ",plag_result)
+
         # Calculating the relevance of the title with essay
         te_relevancy = Title_Essay_Relevancy(model=model_title_essay, essay=essay, title=title)
-        print(te_relevancy)
+        print("Relevancy ",te_relevancy)
 
         # populating essay to mongo
         mongo_col.insert_one({'essay':preprocessed_essay})
--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 from sentence_transformers import util
 import string
-
+import difflib
 
 def Title_Essay_Relevancy(model, title,essay):
 
@@ -9,9 +9,9 @@ def Title_Essay_Relevancy(model, title,essay):
 
     return util.dot_score(title_embedding, essay_embedding).item()
 
-def Preprocess(preprocess_obj, essay):
+def Preprocess(text_process_pipeline, essay):
     # Tokenization, lowercasing, stopword removal, punctuation removal, lemmatization
-    doc = preprocess_obj(essay)
+    doc = text_process_pipeline(essay)
     tokens = [token.lemma_.lower() for token in doc if not token.is_stop and token.text not in string.punctuation]
 
     # Return the preprocessed text as a string
@@ -25,8 +25,34 @@ def plag_calculate(cosine_1g, cosine_2g, cosine_3g, essay:str, db_essay_list:lis
     for db_essay in db_essay_list:
         sum = 0
         # sum+=cosine_1g.similarity(essay,db_essay)
-        # sum+=cosine_2g.similarity(essay,db_essay)
-        sum+=cosine_3g.similarity(essay,db_essay)
+        sum+=cosine_2g.similarity(essay,db_essay)
+        # sum+=cosine_3g.similarity(essay,db_essay)
         plag_results.append(sum)
-    print('max(plag_results) ',max(plag_results))
-    return max(plag_results)
+        plag = max(plag_results)
+    return max(1- plag,0)
+
+def Grammar_Spell_Check(text_process_pipeline, gs_model, gs_model_args, essay, cosine_obj):
+    doc = text_process_pipeline(essay)
+    sentences = [sent.text for sent in doc.sents]
+    corrected_essay = ''
+    for sentence in sentences:
+        corrected_essay+= gs_model.generate_text(f"grammar: {sentence}", args=gs_model_args).text+' '
+
+    print('\n\n' 'corrected_essay\n',corrected_essay)
+
+    # Split the essays into individual words
+    words_corrected_essay = corrected_essay.split()
+    words_essay = essay.split()
+
+    # Calculate the differences using difflib
+    diff = difflib.ndiff(words_corrected_essay, words_essay)
+
+    # Count the number of differences
+    num_differences = sum(1 for _ in diff if not _.startswith(' '))
+
+    # Normalize the number of differences 0 to 1
+    max_length = max(len(words_corrected_essay), len(words_essay))
+    similarity= 1 - (num_differences / max_length)
+
+    return max(similarity, 0)
+    # return cosine_obj.distance(essay,corrected_essay)
