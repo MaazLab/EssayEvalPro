41f3306d3f9892b05f34511596731edaee5cca9d
MaazLab <maaz.rafique.75@gmail.com>
2023-06-08 16:13:11 +0500

    Add TF-IDF for plag and Grammar check

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, request
 from sentence_transformers import SentenceTransformer
-import spacy
 import pymongo
-from utils import Preprocess, Title_Essay_Relevancy,plag_calculate,Grammar_Spell_Check
+from utils import Preprocess,Title_Essay_Relevancy,plag_calculate,Grammar_Spell_Check
 from strsimpy import Cosine
 from happytransformer import HappyTextToText, TTSettings
+
+
 
 # Intialization
 cosine_1g, cosine_2g, cosine_3g = Cosine(1), Cosine(2), Cosine(3)
@@ -14,13 +15,12 @@ mongo_client = pymongo.MongoClient("localhost", 27017)
 mongo_col = mongo_client['EssayEvalPro']['Essay']
 
 # Get All Data From the DB
-essays_list = [essay['essay'] for essay in mongo_col.find({},{"_id":0, 'essay':1})]
+existing_essays = [essay['essay'] for essay in mongo_col.find({},{"_id":0, 'essay':1})]
 
 # Esaay Title Relevancy Model
 model_title_essay = SentenceTransformer('multi-qa-MiniLM-L6-cos-v1')
 
-# Data Processing English language model in spaCy
-nlp = spacy.load("en_core_web_sm") #exclude=['tagger','parser','ner','entity_linker']
+
 
 # Grammar and spelling correction model
 happy_tt = HappyTextToText("T5", "vennify/t5-base-grammar-correction")
@@ -32,6 +32,7 @@ app = Flask(__name__)
 @app.route('/process', methods=['POST'])
 def process_file():
 
+    print(request)
     if 'file' not in request.files:
         return 'Error: No file uploaded.'
 
@@ -45,7 +46,7 @@ def process_file():
     plag_thresh = float(request.form.get('plag_thresh', default=0.5))
     grammar_thresh = float(request.form.get('grammar_thresh', default=0.2))
     title = request.form.get('title', default=None)
-    # print("parameters \n",plag_thresh,grammar_thresh,title)
+    print("parameters \n",plag_thresh,grammar_thresh,title)
 
     try:
         # Read the contents of the file
@@ -56,28 +57,28 @@ def process_file():
         # Extracting title from text file 
         if title is None:
             # Spliting Title and essay
-            split_content = file_content.split('\n\n\n')
-            title = split_content[0]; essay = split_content[1]
-            # print('title ',title)
-            # print('essay ',essay)
-        
+            split_content = file_content.split('\r\n\r\n\r\n')
+            title = split_content[0]; essay = split_content[1]        
         else:
             essay = file_content
-
         #Grammar and Spelling Check
-        grammar_result = Grammar_Spell_Check(text_process_pipeline=nlp, gs_model=happy_tt, gs_model_args=happy_tt_args, cosine_obj=cosine_1g, essay=essay)
+        grammar_result, corrected_essay = Grammar_Spell_Check( gs_model=happy_tt, gs_model_args=happy_tt_args, cosine_obj=cosine_1g, essay=essay)
+        
+        print('corrected_essay ',corrected_essay)
         print("grammar_result ",grammar_result)
+        
+        # preprocessing document for DB and plag
+        preprocessed_essay = Preprocess(essay=corrected_essay)
 
-        # Preprocessing for plagiarism calaculcation
-        preprocessed_essay = Preprocess(nlp,essay)
-        
         # Plag result high result means low plag
-        if len(essays_list) != 0:
-            plag_result = plag_calculate(cosine_1g, cosine_2g, cosine_3g, essay, db_essay_list=essays_list)
+        if len(existing_essays) != 0:
+            print("type(preprocessed_essay) ",type(preprocessed_essay))
+            print("type(existing_essays) ",type(existing_essays))
+            matched_essay, plag_score = plag_calculate(essay=preprocessed_essay, essay_list=existing_essays)
         else:
-            plag_result = 1
+            plag_score = 1
         
-        print("plag result ",plag_result)
+        print("plag result ",plag_score)
 
         # Calculating the relevance of the title with essay
         te_relevancy = Title_Essay_Relevancy(model=model_title_essay, essay=essay, title=title)
@@ -85,13 +86,13 @@ def process_file():
 
         # populating essay to mongo
         mongo_col.insert_one({'essay':preprocessed_essay})
-        essays_list.append(preprocessed_essay)
+        existing_essays.append(preprocessed_essay)
 
 
-        return str(plag_result)
+        return str(plag_score)
     
     except Exception as e:
         return f'Error: {str(e)}'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
--- utils.py
+++ utils.py
@@ -1,6 +1,12 @@
 from sentence_transformers import util
 import string
 import difflib
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+import spacy
+
+# Data Processing English language model in spaCy
+nlp = spacy.load("en_core_web_sm")
 
 def Title_Essay_Relevancy(model, title,essay):
 
@@ -9,50 +15,59 @@ def Title_Essay_Relevancy(model, title,essay):
 
     return util.dot_score(title_embedding, essay_embedding).item()
 
-def Preprocess(text_process_pipeline, essay):
+def Preprocess(essay):
     # Tokenization, lowercasing, stopword removal, punctuation removal, lemmatization
-    doc = text_process_pipeline(essay)
+    doc = nlp(essay)
     tokens = [token.lemma_.lower() for token in doc if not token.is_stop and token.text not in string.punctuation]
 
     # Return the preprocessed text as a string
     preprocessed_text = ' '.join(tokens)
     return preprocessed_text
 
-def plag_calculate(cosine_1g, cosine_2g, cosine_3g, essay:str, db_essay_list:list):
-    print('Total essays in DB ',type(db_essay_list))
-    # Create list to save result with every essay
-    plag_results = []
-    for db_essay in db_essay_list:
-        sum = 0
-        # sum+=cosine_1g.similarity(essay,db_essay)
-        sum+=cosine_2g.similarity(essay,db_essay)
-        # sum+=cosine_3g.similarity(essay,db_essay)
-        plag_results.append(sum)
-        plag = max(plag_results)
-    return max(1- plag,0)
+def DotProduct_calculate(essay, essay_list):
 
-def Grammar_Spell_Check(text_process_pipeline, gs_model, gs_model_args, essay, cosine_obj):
-    doc = text_process_pipeline(essay)
+    # Create TF-IDF vectors for all essays
+    vectorizer = TfidfVectorizer()
+    if type(essay_list) is str:
+        tfidf_matrix = vectorizer.fit_transform([essay_list,essay])
+    else:
+        tfidf_matrix = vectorizer.fit_transform(essay_list + [essay])
+    tfidf_matrix = tfidf_matrix.toarray()
+
+    # Calculate cosine similarity between input essay and existing essays
+    similarities = np.dot(tfidf_matrix, tfidf_matrix[-1].T)
+    sorted_indices = np.argsort(similarities, axis=0)[::-1]
+
+    return similarities, sorted_indices
+
+def plag_calculate(essay, essay_list):
+
+    max_plag_score = 0
+    max_plag_essay = ""
+
+    # Create Vectors and Calculate DotProduct
+    similarities, sorted_indices = DotProduct_calculate(essay=essay, essay_list=essay_list)
+
+    # Find the essay with the maximum plagiarism score
+    for idx in sorted_indices[1:]:
+        if idx < len(essay_list):
+            plag_score = similarities[idx]
+            if plag_score > max_plag_score:
+                max_plag_score = plag_score
+                max_plag_essay = essay_list[idx]
+
+    # Adjust the maximum plagiarism score to be between 0 and 1
+    max_plag_score = max(0, min(max_plag_score, 1))
+
+    return max_plag_essay, max_plag_score
+
+def Grammar_Spell_Check(gs_model, gs_model_args, essay, cosine_obj):
+    doc = nlp(essay)
     sentences = [sent.text for sent in doc.sents]
     corrected_essay = ''
     for sentence in sentences:
         corrected_essay+= gs_model.generate_text(f"grammar: {sentence}", args=gs_model_args).text+' '
 
-    print('\n\n' 'corrected_essay\n',corrected_essay)
-
-    # Split the essays into individual words
-    words_corrected_essay = corrected_essay.split()
-    words_essay = essay.split()
-
-    # Calculate the differences using difflib
-    diff = difflib.ndiff(words_corrected_essay, words_essay)
-
-    # Count the number of differences
-    num_differences = sum(1 for _ in diff if not _.startswith(' '))
-
-    # Normalize the number of differences 0 to 1
-    max_length = max(len(words_corrected_essay), len(words_essay))
-    similarity= 1 - (num_differences / max_length)
-
-    return max(similarity, 0)
-    # return cosine_obj.distance(essay,corrected_essay)
+    similarities, _ = DotProduct_calculate(essay=essay, essay_list=corrected_essay)
+    print('similarities ',similarities)
+    return min(similarities), corrected_essay
